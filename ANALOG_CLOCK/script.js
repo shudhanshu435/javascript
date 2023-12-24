78bfecffc0bb41449d9b6d6d6e958765b4ab0f36
shudhanshu <sudhanshukumar1242002@gmail.com>
2023-12-24 13:35:27 +0530

    Initial commit

---
+++ ANALOG_CLOCK/script.js
@@ -0,0 +1,20 @@
+let hr = document.getElementById('hour')
+let min = document.getElementById('min')
+let sec = document.getElementById('sec')
+
+function displayTime(){
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hourRotation = 30*hh + mm/2;
+    let minRotation = 6*mm;
+    let secRotation = 6*ss;
+
+    hr.style.transform = `rotate(${hourRotation}deg)`;
+    min.style.transform = `rotate(${minRotation}deg)`;
+    sec.style.transform = `rotate(${secRotation}deg)`;
+}
+
+setInterval(displayTime, 1000);
